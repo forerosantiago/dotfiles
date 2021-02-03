6a30994afbe858b9c08915bb6168a050dea70022
Santiago Forero <thesantiagoforero@gmail.com>
2021-02-03 01:05:22 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 echo ".cfg" >> .gitignore
 git clone --bare https://github.com/forerosantiago/dotfiles/ $HOME/.cfg
 
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config switch -f main
 
 config config --local status.showUntrackedFiles no
