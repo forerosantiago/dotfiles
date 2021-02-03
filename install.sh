ec7d6e9940b2329265599ad5f76029f88199bf82
Santiago Forero <64563594+forerosantiago@users.noreply.github.com>
2021-02-02 20:54:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-alias config="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
-source ~/.zshrc
+alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 rm -rf .cfg
 echo ".cfg" >> .gitignore
-git clone --bare https://www.github.com/forerosantiago/dotfiles $HOME/.cfg
+git clone --bare https://github.com/forerosantiago/dotfiles $HOME/.cfg
 config checkout -f main
 config config --local status.showUntrackedFiles no
